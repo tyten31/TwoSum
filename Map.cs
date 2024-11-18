c96e23519701021151e42cde0941b82f1cfe444a
ttenney <ttenney@arrowinternational.com>
2024-11-18 14:20:16 -0800

    TwoSum

---
+++ Map.cs
@@ -0,0 +1,9 @@
+﻿namespace TwoSum
+{
+    internal class Map
+    {
+        public int Index { get; set; }
+
+        public int Value { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,28 +1,65 @@
-﻿namespace SortTool
+﻿namespace TwoSum
 {
     internal class Program
     {
         public static async Task Main(string[] args)
         {
-            int target = 9;
-            int[] nums = { 2, 7, 11, 15 };
+            //int target = 9;
+            //int[] nums = [2, 7, 11, 15];
+
+            //int target = 6;
+            //int[] nums = [3, 2, 4];
+
+            int target = 11;
+            int[] nums = [1, 1, 1, 1, 1, 4, 1, 1, 1, 1, 1, 7, 1, 1, 1, 1, 1];
 
             var result = TwoSum(nums, target);
-            Console.WriteLine();
+
+            Console.WriteLine($"Target: {target}");
+            Console.Write("Nums: [");
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i == nums.Length - 1)
+                {
+                    Console.Write($"{nums[i]}");
+                }
+                else
+                {
+                    Console.Write($"{nums[i]},");
+                }
+            }
+
+            Console.WriteLine("]");
+            Console.WriteLine($"[{result[0]},{result[1]}]");
+            Console.ReadLine();
         }
 
         public static int[] TwoSum(int[] nums, int target)
         {
-            // Step 1: Remove any array element that is larger than the target
+            var result = new int[2];
+            var maps = new List<Map>();
 
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var current = new Map { Index = i, Value = nums[i] };
+                var diff = target - current.Value;
+                var foundMap = maps.Find(x => x.Value == diff);
 
+                if (foundMap == null)
+                {
+                    maps.Add(current);
+                }
+                else
+                {
+                    result[0] = foundMap.Index;
+                    result[1] = i;
 
+                    break;
+                }
+            }
 
-
-
-
-
-            return new int[] { nums[target] };
+            return result;
         }
     }
 }
