ec2a148136e5ac0fabc47efdb5e04cbe61b00bed
ttenney <ttenney@arrowinternational.com>
2024-11-18 13:24:39 -0800

    TwoSum

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace SortTool
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            int target = 9;
+            int[] nums = { 2, 7, 11, 15 };
+
+            var result = TwoSum(nums, target);
+            Console.WriteLine();
+        }
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            // Step 1: Remove any array element that is larger than the target
+
+
+
+
+
+
+
+
+            return new int[] { nums[target] };
+        }
+    }
+}
